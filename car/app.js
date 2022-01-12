324c7042dba7763627538946a8f94f9cdb9750cf
Apple <apple@Apples-MacBook-Pro.local>
2022-01-12 11:49:28 +0530

    format code

--- car/app.js
+++ car/app.js
@@ -1,364 +1,322 @@
 /******************* VARIABLE DEFINITION *************************/
 //Constants
-let ROW = 30, COL =20;
-let EMPTY = 0, CAR = 1, RCAR = 2 ;
-let CENTER = 9, LEFT = 3, RIGHT = 15;
+let ROW = 30,
+  COL = 20;
+let EMPTY = 0,
+  CAR = 1,
+  RCAR = 2;
+let CENTER = 9,
+  LEFT = 3,
+  RIGHT = 15;
 
 //Key states
-let KEY_LEFT = 37 , KEY_RIGHT = 39, KEY_UP = 38, ESC = 27;
+let KEY_LEFT = 37,
+  KEY_RIGHT = 39,
+  KEY_UP = 38,
+  ESC = 27;
 
 //speed variables
-let FAST = 2, NORMAL = 8;
+let FAST = 2,
+  NORMAL = 8;
 
 //Game variables
-let canvas, ctx, score, frames, keystate, currFrame , speed;
-let carpos = CENTER ;
+let canvas, ctx, score, frames, keystate, currFrame, speed;
+let carpos = CENTER;
 let gamefail = 0;
 let count = 0;
 
 //Score variables
-let carcrossed ;
+let carcrossed;
 let startTime, endTime;
 
 //Main variables
-class Map{
-	constructor() {
-		this.width= null;
-		this.height= null;
-		this._grid= null;
-	}
-	
+class Map {
+  constructor() {
+    this.width = null;
+    this.height = null;
+    this._grid = null;
+  }
 
-	init(d) {
-		this.width = COL;
-		this.height = ROW*20;
+  init(d) {
+    this.width = COL;
+    this.height = ROW * 20;
 
-		this._grid = [] ;
+    this._grid = [];
 
-		for (let i = 0; i < this.width; i++) {
-			this._grid.push([]);
-			for (let j = 0; j < this.height; j++) {
-				this._grid[i].push(d);
-			};			
-		};
+    for (let i = 0; i < this.width; i++) {
+      this._grid.push([]);
+      for (let j = 0; j < this.height; j++) {
+        this._grid[i].push(d);
+      }
+    }
 
-		let posx = [LEFT, CENTER, RIGHT ];
-		let posyprev = 0;
-		let posxprev = 1;
-		let ydiff = [8 ,9,10];
-		let randx, randy;
-		while ( posyprev < this.height - 20 ) {
-			do
-			{
-				randx = Math.floor(Math.random()*3);
-			}while (randx == posxprev);	
-			posxprev = randx;
-			randy = Math.floor(Math.random()*3);
-			setCar( this, CAR, posx[randx], posyprev + ydiff[randy] );
-			posyprev = posyprev + ydiff[randy] ;
-		}
-	}
+    let posx = [LEFT, CENTER, RIGHT];
+    let posyprev = 0;
+    let posxprev = 1;
+    let ydiff = [8, 9, 10];
+    let randx, randy;
+    while (posyprev < this.height - 20) {
+      do {
+        randx = Math.floor(Math.random() * 3);
+      } while (randx == posxprev);
+      posxprev = randx;
+      randy = Math.floor(Math.random() * 3);
+      setCar(this, CAR, posx[randx], posyprev + ydiff[randy]);
+      posyprev = posyprev + ydiff[randy];
+    }
+  }
 
-	set( val, x, y ){
-		this._grid[x][y] = val;
-	}
+  set(val, x, y) {
+    this._grid[x][y] = val;
+  }
 
-	get( x,y ) {
-		return this._grid[x][y];
-	}
-
+  get(x, y) {
+    return this._grid[x][y];
+  }
 }
 
 let map = new Map();
 
-class Grid{
-	constructor() {
-		this.width= null;
-		this.height= null;
-		this._grid= null;
-	}
+class Grid {
+  constructor() {
+    this.width = null;
+    this.height = null;
+    this._grid = null;
+  }
 
-	init(d,c,r){
-		this.width = COL;
-		this.height = ROW;
+  init(d, c, r) {
+    this.width = COL;
+    this.height = ROW;
 
-		this._grid =[];
+    this._grid = [];
 
-		for (let i = 0; i < c; i++) {
-			this._grid.push([]);
-			for (let j = 0; j < r; j++) {
-				this._grid[i].push(d);
-				};			
-			};
-	}
+    for (let i = 0; i < c; i++) {
+      this._grid.push([]);
+      for (let j = 0; j < r; j++) {
+        this._grid[i].push(d);
+      }
+    }
+  }
 
-	set( val, x, y ){
-		this._grid[x][y] = val;
-	}
+  set(val, x, y) {
+    this._grid[x][y] = val;
+  }
 
-	get( x,y ) {
-		return this._grid[x][y];
-	}
-
+  get(x, y) {
+    return this._grid[x][y];
+  }
 }
 
 let grid = new Grid();
 
 /*************** FUNCTION DEFINITIONS **********************/
-function mainScreen()
-{
-	let div = document.getElementById("canvas");
-	if( div ) {document.body.removeChild( div );}
+function mainScreen() {
+  let div = document.getElementById("canvas");
+  if (div) {
+    document.body.removeChild(div);
+  }
 
-	mainDiv = document.createElement("div");
-	mainDiv.classList.add("mainScreen");
-	mainDiv.id = "mainScreen";
-	document.body.appendChild( mainDiv );
+  mainDiv = document.createElement("div");
+  mainDiv.classList.add("mainScreen");
+  mainDiv.id = "mainScreen";
+  document.body.appendChild(mainDiv);
 
-	startButton = document.createElement("button");
-	startButton.classList.add("start")
-	startButton.innerHTML = "Start";
-	startButton.addEventListener("click", function(){
-		clearScreen();
-		game();
-		});
-	mainDiv.appendChild( startButton );
+  startButton = document.createElement("button");
+  startButton.classList.add("start");
+  startButton.innerHTML = "Start";
+  startButton.addEventListener("click", function () {
+    clearScreen();
+    game();
+  });
+  mainDiv.appendChild(startButton);
 }
 
-function clearScreen()
-{
-	let div = document.getElementById("mainScreen");
-	document.body.removeChild( div );
+function clearScreen() {
+  let div = document.getElementById("mainScreen");
+  document.body.removeChild(div);
 }
 
-function setCar( place, type, x, y )
-{
-	for (let i = 0; i < 3; i++) {
-		for (let j = 0; j < 4; j++) {
-			if( !( ( i==0 || i==2 ) && ( j==0 || j==2 ) ) )  {
-				place.set(type, x+i, y+j);	
-			}
-		}	
-	}
+function setCar(place, type, x, y) {
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (!((i == 0 || i == 2) && (j == 0 || j == 2))) {
+        place.set(type, x + i, y + j);
+      }
+    }
+  }
 }
 
-function createCanvas()
-{
-	canvas = document.createElement("canvas");
-	canvas.id = "canvas";
-	canvas.classList.add("grid");
-	canvas.width = COL*20;
-	canvas.height = ROW*20;
-	ctx = canvas.getContext("2d");
-	document.body.appendChild(canvas);
+function createCanvas() {
+  canvas = document.createElement("canvas");
+  canvas.id = "canvas";
+  canvas.classList.add("grid");
+  width = COL * 20;
+  height = ROW * 20;
+  ctx = canvas.getContext("2d");
+  document.body.appendChild(canvas);
 }
 
-function getKey()
-{
+function getKey() {
+  keystate = [];
+  document.addEventListener("keydown", function (evt) {
+    keystate[evt.keyCode] = true;
+  });
 
-	keystate = [];
-	document.addEventListener( "keydown", function( evt ){
-		keystate[evt.keyCode] = true;
-	} );
-
-	document.addEventListener( "keyup", function( evt ){
-		delete keystate[evt.keyCode];
-	} )
-
-
+  document.addEventListener("keyup", function (evt) {
+    delete keystate[evt.keyCode];
+  });
 }
 
-function game()
-{
-	//initialise the game
-	init();
+function setup() {
+  //initialise the game
+  init();
 
-	//map + cars
-	createCanvas();
-
-	//get key pressed
-	getKey();
-
-	//all the cool stuff happens here
-	loop();
+  //map + cars
+  createCanvas(COL * 20, ROW * 20);
+  background(143, 200, 45);
 }
 
-function loop()
-{
-	if( keystate[ESC] ) { 
-		endGame();
-		mainScreen();
-	}
-	update();
-	if( !gamefail ) { window.requestAnimationFrame( loop, canvas ); }
-	
+function draw() {
+  update();
 }
 
-function update()
-{
-if( !gamefail )
-	{
-		frames++;
-		let carposNew = carpos;
-		
-		//to reduce sensitivity of keys being presed
-		if( frames - currFrame > 6 )
-		{
-		//get key pressed and new position
-			if (keystate[KEY_LEFT] && carpos == RIGHT ) { 
-				carposNew = CENTER;
-				currFrame = frames; }
-			if (keystate[KEY_LEFT] && carpos == CENTER ) { 
-				carposNew = LEFT;
-				currFrame = frames; }
-			if (keystate[KEY_RIGHT] && carpos == CENTER ) { 
-				carposNew = RIGHT;
-				currFrame = frames; }
-			if (keystate[KEY_RIGHT] && carpos == LEFT ) { 
-				carposNew = CENTER;
-				currFrame = frames; }
-		}
+function update() {
+  if (!gamefail) {
+    frames++;
+    let carposNew = carpos;
 
-		//control speed
-		if( keystate[KEY_UP] ) { speed = FAST;}
-		else{ speed = NORMAL; }
+    //to reduce sensitivity of keys being presed
+    if (frames - currFrame > 6) {
+      //get key pressed and new position
+      if (keyCode == LEFT_ARROW && carpos == RIGHT) {
+        carposNew = CENTER;
+        currFrame = frames;
+      }
+      if (keyCode == LEFT_ARROW && carpos == CENTER) {
+        carposNew = LEFT;
+        currFrame = frames;
+      }
+      if (keyCode == RIGHT_ARROW && carpos == CENTER) {
+        carposNew = RIGHT;
+        currFrame = frames;
+      }
+      if (keyCode == RIGHT_ARROW && carpos == LEFT) {
+        carposNew = CENTER;
+        currFrame = frames;
+      }
+    }
 
-		if( keystate[KEY_LEFT] || keystate[KEY_RIGHT] )
-		{
-		//set car in current position EMPTY and ser new RCAR
-		setCar( grid, EMPTY, carpos, ( ROW - 5 ) );
-		setCar( grid, RCAR, carposNew, ( ROW - 5 ) );
-		}
+    //control speed
+    if (keyCode == UP_ARROW) {
+      speed = FAST;
+    } else {
+      speed = NORMAL;
+    }
 
-		carpos = carposNew;
-		
-		draw( count );
-		check( count );
-		
-		//move the map
-		if( frames%speed == 0 )
-		{
-			count++ ;
-			calcscore( count );
-		}
+    if (keyCode == RIGHT_ARROW || keyCode == LEFT_ARROW) {
+      //set car in current position EMPTY and ser new RCAR
+      setCar(grid, EMPTY, carpos, ROW - 5);
+      setCar(grid, RCAR, carposNew, ROW - 5);
+    }
 
-		//end game if map is complete
-		if( count > map.height + 10 )
-		{
-			endGame();
-			mainScreen();
-		}
+    carpos = carposNew;
 
-	}
-else
-{
-	init();
-	gamefail = 0;
+    drawCar(count);
+    check(count);
+
+    //move the map
+    if (frameCount % speed == 0) {
+      count++;
+      calcscore(count);
+    }
+
+    //end game if map is complete
+    if (count > map.height + 10) {
+      endGame();
+      // mainScreen();
+    }
+  } else {
+    init();
+    gamefail = 0;
+  }
 }
 
+function check(count) {
+  for (let i = 0; i < grid.width; i++) {
+    for (let j = 0; j < grid.height; j++) {
+      if (
+        grid.get(i, j) + map.get(i, j + (map.height - grid.height - count)) ==
+        3
+      ) {
+        endGame();
+        break;
+      }
+    }
+  }
 }
 
-function check( count )
-{
-
-for (let i = 0; i < grid.width; i++) {
-	for (let j = 0; j < grid.height; j++) {
-		
-		if( ( grid.get(i,j) + map.get(i,j + ( map.height - grid.height - count ) ) )==3 )	
-			{
-				endGame();
-				break;
-			}
-	}
-	
-}	
-
+function endGame() {
+  gamefail = 1;
+  // mainScreen();
 }
 
-function endGame()
-{
-	gamefail = 1;
-	mainScreen();
+function calcscore(count) {
+  for (let i = 0; i < grid.width; i++) {
+    if (map.get(i, map.height - grid.height - count) == 1) {
+      carcrossed++;
+      console.log(i + "--" + (map.height - grid.height - count));
+    }
+  }
 }
 
-function calcscore( count )
-{
+function drawCar(count) {
+  tw = width / grid.width;
+  th = height / grid.height;
 
-for (let i = 0; i < grid.width; i++) {
-	if( map.get( i, ( map.height - grid.height - count ) ) == 1)
-	{
-		carcrossed++;
-		console.log( i + "--" + ( map.height- grid.height - count ))		
-	}
-		
-}		
-
+  for (let i = 0; i < grid.width; i++) {
+    for (let j = 0; j < grid.height; j++) {
+      switch (
+        grid.get(i, j) + map.get(i, j + (map.height - grid.height - count))
+      ) {
+        case EMPTY:
+          fill("#ffffff");
+          rect(i * tw, j * th, tw, th);
+          break;
+        case CAR:
+          fill("#660033");
+          rect(i * tw, j * th, tw, th);
+          break;
+        case RCAR:
+          fill("#D699AE");
+          rect(i * tw, j * th, tw, th);
+          break;
+        case 3:
+          fill("#000000");
+          rect(i * tw, j * th, tw, th);
+      }
+    }
+  }
+  fill("#000");
+  // ctx.font =  "12px Arial";
+  text("CARS CROSSED : " + Math.floor(carcrossed / 8), 10, height - 10);
+  currTime = new Date().getTime() / 1000;
+  text("TIME : " + Math.floor(currTime - startTime), 150, height - 10);
 }
 
-
-function draw( count )
-{
-tw = canvas.width/grid.width;
-th = canvas.height/grid.height;
-
-for (let i = 0; i < grid.width; i++) {
-		for (let j = 0; j < grid.height; j++) {
-			switch( grid.get(i,j) + map.get(i,j + ( map.height - grid.height - count ) ) ){
-
-			case EMPTY:
-				ctx.fillStyle = "#ffffff";
-				ctx.fillRect(i*tw, j*th , tw,th);	
-				break;
-			case CAR:
-				ctx.fillStyle = "#660033";
-				ctx.fillRect(i*tw, j*th , tw,th);
-				break;
-			case RCAR:
-				ctx.fillStyle = "#D699AE";
-				ctx.fillRect(i*tw, j*th , tw,th);
-				break
-			case 3:
-				ctx.fillStyle = "#000000";
-				ctx.fillRect(i*tw, j*th , tw,th);
-				
-			}
-			
-		}	
-	}
-	ctx.fillStyle = "#000";
-	ctx.font =  "12px Arial";
-	ctx.fillText( ( "CARS CROSSED : " + Math.floor( carcrossed/8 ) ) , 10 , ( canvas.height - 10 ) );
-	currTime = new Date().getTime() / 1000;
-	ctx.fillText(  "TIME : " + ( Math.floor(currTime - startTime) ) , 150 , ( canvas.height - 10 ) );
+function setvariables() {
+  // to initialise game variables
+  count = 0;
+  frames = 0;
+  currFrame = 0;
+  speed = NORMAL;
+  carpos = CENTER;
+  carcrossed = 0;
+  startTime = new Date().getTime() / 1000;
 }
 
-function setvariables()
-{
-// to initialise game variables
-	count = 0 ;
-	frames = 0;
-	currFrame = 0;
-	speed = NORMAL;
-	carpos =CENTER;
-	carcrossed = 0;
-	startTime = new Date().getTime() / 1000;
+function init() {
+  setvariables();
+  grid.init(EMPTY, COL, ROW);
+  setCar(grid, RCAR, carpos, ROW - 5);
+  map.init(EMPTY);
 }
-
-function init()
-{
-	setvariables();
-	grid.init( EMPTY, COL, ROW );
-	setCar( grid, RCAR, carpos, ( ROW - 5 ) );
-	map.init( EMPTY );
-}
-
-
-/******************* MAIN *************************/
-
-function main()
-{
-// Call main game function
-	mainScreen();
-}
-
-window.onload = main ;
--- snake/app.js
+++ snake/app.js
@@ -1,262 +1,274 @@
 /******************************** letIABLE DEFINITIONS *********************/
 //Constants
-let row = 26, col =26;
-let EMPTY = 0, SNAKE = 1, FOOD = 2, NUMNUM = 3;
+let row = 26,
+  col = 26;
+let EMPTY = 0,
+  SNAKE = 1,
+  FOOD = 2,
+  NUMNUM = 3;
 
 //Directions
-let UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3;
+let UP = 0,
+  DOWN = 1,
+  LEFT = 2,
+  RIGHT = 3;
 
 //Key states
-let KEY_LEFT = 37 , KEY_UP = 38, KEY_RIGHT = 39, KEY_DOWN = 40;
+let KEY_LEFT = 37,
+  KEY_UP = 38,
+  KEY_RIGHT = 39,
+  KEY_DOWN = 40;
 
 //Game objects
-let canvas, ctx,keystate, frames, score;
+let canvas, ctx, keystate, frames, score;
 
 // All let for numnum
-let startTime, currTime, numx = 0, numy = 0;
+let startTime,
+  currTime,
+  numx = 0,
+  numy = 0;
 
 //Food pictures
 let cherry, grapes;
 
 function preload() {
-	cherry = loadImage('cherries.png');
-	grapes = loadImage('grapes.jpeg')
+  cherry = loadImage("cherries.png");
+  grapes = loadImage("grapes.jpeg");
 }
 
-
 //Main variables
-class Grid{
-	constructor() {
-		this.width= null;
-		this.height= null;
-		this._grid= null;
-	}
-	
-	init(d,c,r){
-		this.width = col;
-		this.height = row;
+class Grid {
+  constructor() {
+    this.width = null;
+    this.height = null;
+    this._grid = null;
+  }
 
-		this._grid =[];
+  init(d, c, r) {
+    this.width = col;
+    this.height = row;
 
-		for (let i = 0; i < col; i++) {
-			this._grid.push([]);
-			for (let j = 0; j < row; j++) {
-				this._grid[i].push(d);
-				};			
-			};
-	}
-	
-	set( val, x, y ){
-		this._grid[x][y] = val;
-	}
+    this._grid = [];
 
-	get( x,y ) {
-		return this._grid[x][y];
-	}
+    for (let i = 0; i < col; i++) {
+      this._grid.push([]);
+      for (let j = 0; j < row; j++) {
+        this._grid[i].push(d);
+      }
+    }
+  }
+
+  set(val, x, y) {
+    this._grid[x][y] = val;
+  }
+
+  get(x, y) {
+    return this._grid[x][y];
+  }
 }
 
 let grid = new Grid();
 
-class Snake{
+class Snake {
+  constructor() {
+    this.direction = null;
+    this.last = null;
+    this._queue = null;
+  }
 
-	constructor() {
-		this.direction= null;
-		this.last= null;
-		this._queue= null;
-	}
-	
+  init(d, x, y) {
+    this.direction = d;
+    this._queue = [];
+    this.insert(x, y);
+  }
 
-	init(d,x,y){
-		this.direction = d;
-		this._queue = [];
-		this.insert(x,y);
+  insert(x, y) {
+    this._queue.unshift({ x: x, y: y });
+    this.last = this._queue[0];
+  }
 
-	}
-
-	insert(x, y ){
-		this._queue.unshift({x:x, y:y});
-		this.last = this._queue[0];
-
-	}
-
-	remove(){
-		return this._queue.pop();
-	}
-
+  remove() {
+    return this._queue.pop();
+  }
 }
 
 let snake = new Snake();
 
 /****************** FUNCTION DEFINITIONS **********************/
 
-
-function setFood( type ) {
-let empty = [];
-	for (let i = 0; i < grid.width; i++) {
-		for (let j = 0; j < grid.height; j++) {
-			if( grid.get(i,j) == EMPTY ){
-				empty.push({x:i, y:j});
-			}
-		}	
-
-	}
-	let randpos = empty[Math.floor(Math.random()*empty.length)];
-	grid.set( type, randpos.x, randpos.y);
-	return randpos;
+function setFood(type) {
+  let empty = [];
+  for (let i = 0; i < grid.width; i++) {
+    for (let j = 0; j < grid.height; j++) {
+      if (grid.get(i, j) == EMPTY) {
+        empty.push({ x: i, y: j });
+      }
+    }
+  }
+  let randpos = empty[Math.floor(Math.random() * empty.length)];
+  grid.set(type, randpos.x, randpos.y);
+  return randpos;
 }
 
-function getKey()
-{
-	keystate = [];
-	document.addEventListener( "keydown" , function( evt ) {
-		keystate[evt.keyCode] = true;
-	} );
-	document.addEventListener( "keyup" , function( evt ) {
-		delete keystate[evt.keyCode];
-	} );
+function getKey() {
+  keystate = [];
+  document.addEventListener("keydown", function (evt) {
+    keystate[evt.keyCode] = true;
+  });
+  document.addEventListener("keyup", function (evt) {
+    delete keystate[evt.keyCode];
+  });
 }
 
 function moveSnake() {
-	let tail;
-	
-	// frames++ ;
-	
-	if(keyIsPressed) {
-	//change direction of the snake
-		if (keyCode == LEFT_ARROW && snake.direction != RIGHT) { snake.direction = LEFT; }
-		if (keyCode == RIGHT_ARROW && snake.direction != LEFT ) { snake.direction = RIGHT; }
-		if (keyCode == DOWN_ARROW && snake.direction != UP) { snake.direction = DOWN; }
-		if (keyCode == UP_ARROW && snake.direction != DOWN) { snake.direction = UP; }
-	}
-	//automatically move the snake
-	if( frameCount%3 == 0){
-		let nx = snake.last.x;
-		let ny = snake.last.y;
-		switch( snake.direction ){
-			case LEFT:
-				if(nx<=0) {nx = (col - nx -1 )%col;}
-				else { nx = (nx - 1 )%col;}
-				break;
-			case RIGHT:
-				nx = (nx+1)%col;
-				break;
-			case UP:
-				if(ny<=0) {ny = (row - ny -1 )%row;}
-				else { ny = (ny - 1 )%row;}
-				break;
-			case DOWN:
-				ny = (ny + 1 )%row;			
-				break;
-		}
+  let tail;
 
-		
-	//Move snake based on food/snake/empty
+  // frames++ ;
 
-		//if FOOD/NUMNUM
-		if( (grid.get(nx,ny) == FOOD) ||( grid.get(nx,ny) == NUMNUM ) )
-		{
-			tail = {x:nx, y:ny};
+  if (keyIsPressed) {
+    //change direction of the snake
+    if (keyCode == LEFT_ARROW && snake.direction != RIGHT) {
+      snake.direction = LEFT;
+    }
+    if (keyCode == RIGHT_ARROW && snake.direction != LEFT) {
+      snake.direction = RIGHT;
+    }
+    if (keyCode == DOWN_ARROW && snake.direction != UP) {
+      snake.direction = DOWN;
+    }
+    if (keyCode == UP_ARROW && snake.direction != DOWN) {
+      snake.direction = UP;
+    }
+  }
+  //automatically move the snake
+  if (frameCount % 3 == 0) {
+    let nx = snake.last.x;
+    let ny = snake.last.y;
+    console.log(nx, ny);
+    console.log(snake);
+    switch (snake.direction) {
+      case LEFT:
+        if (nx <= 0) {
+          nx = (col - nx - 1) % col;
+        } else {
+          nx = (nx - 1) % col;
+        }
+        break;
+      case RIGHT:
+        nx = (nx + 1) % col;
+        break;
+      case UP:
+        if (ny <= 0) {
+          ny = (row - ny - 1) % row;
+        } else {
+          ny = (ny - 1) % row;
+        }
+        break;
+      case DOWN:
+        ny = (ny + 1) % row;
+        break;
+    }
 
-			if(grid.get(nx,ny) == NUMNUM) { score = score +2 ;}
-			else{ score++; }
+    //Move snake based on food/snake/empty
 
-			if( score%5 == 0) {
-				foodPos = setFood( NUMNUM ); 
+    //if FOOD/NUMNUM
+    if (grid.get(nx, ny) == FOOD || grid.get(nx, ny) == NUMNUM) {
+      tail = { x: nx, y: ny };
 
-				startTime = new Date().getTime() / 1000;
+      if (grid.get(nx, ny) == NUMNUM) {
+        score = score + 2;
+      } else {
+        score++;
+      }
 
-				numx = foodPos.x;
-				numy = foodPos.y;
+      if (score % 5 == 0) {
+        foodPos = setFood(NUMNUM);
 
-				}
-			else{ setFood( FOOD ); }
-		}
+        startTime = new Date().getTime() / 1000;
 
-		// if snake has suicidal tendencies - kill the snake
-		else if( grid.get(nx,ny) == SNAKE )
-		{
-			init();
-			tail = snake.remove();
-		}
+        numx = foodPos.x;
+        numy = foodPos.y;
+      } else {
+        setFood(FOOD);
+      }
+    }
 
-		// if nothing, just move on
-		else
-		{
-			tail = snake.remove();
-			grid.set( EMPTY, tail.x, tail.y);
-			tail.x = nx;
-			tail.y = ny;
-		}
+    // if snake has suicidal tendencies - kill the snake
+    else if (grid.get(nx, ny) == SNAKE) {
+      init();
+      tail = snake.remove();
+    }
 
-	//Remove numnum if > 3s and set normal food item
-		currTime = new Date().getTime() / 1000;
+    // if nothing, just move on
+    else {
+      tail = snake.remove();
+      grid.set(EMPTY, tail.x, tail.y);
+      tail.x = nx;
+      tail.y = ny;
+    }
 
-		if( grid.get(numx,numy) == NUMNUM )
-		{
-			if ( currTime - startTime >2 ){
+    //Remove numnum if > 3s and set normal food item
+    currTime = new Date().getTime() / 1000;
 
-				grid.set( EMPTY, numx, numy );
-				setFood( FOOD );
-			}
-		}
+    if (grid.get(numx, numy) == NUMNUM) {
+      if (currTime - startTime > 2) {
+        grid.set(EMPTY, numx, numy);
+        setFood(FOOD);
+      }
+    }
 
-	//finally set the SNAKE and GRID
-		grid.set( SNAKE, tail.x, tail.y );
-		snake.insert(tail.x, tail.y);
-	}
-
+    //finally set the SNAKE and GRID
+    grid.set(SNAKE, tail.x, tail.y);
+    snake.insert(tail.x, tail.y);
+  }
 }
 
 function drawSnake() {
+  console.log(width);
+  let tw = width / grid.width;
+  let th = height / grid.height;
 
-	console.log(width);
-	let tw = width/grid.width;
-	let th = height/grid.height;
-	
-	for (let i = 0; i < grid.width; i++) {
-		for (let j = 0; j < grid.height; j++) {
-			switch( grid.get(i,j)){
-
-			case EMPTY:
-				fill("#fff");
-				rect(i*tw, j*th , tw,th);	
-				break;
-			case SNAKE:
-				fill("#00F210");
-				rect(i*tw, j*th , tw,th);
-				break;
-			case FOOD:
-				image(cherry,i*tw, j*th , tw,th );
-  				break;
-			case NUMNUM:
-				image(grapes,i*tw, j*th , tw,th );
-				break;
-			}
-			
-		}	
-	}	
-	fill("#000");
-	// ctx.font =  "12px Arial";
-	text( ( "SCORE : " + score ) , 10 , ( height - 10 ) );
+  for (let i = 0; i < grid.width; i++) {
+    for (let j = 0; j < grid.height; j++) {
+      switch (grid.get(i, j)) {
+        case EMPTY:
+          fill("#fff");
+          rect(i * tw, j * th, tw, th);
+          break;
+        case SNAKE:
+          fill("#00F210");
+          rect(i * tw, j * th, tw, th);
+          break;
+        case FOOD:
+          image(cherry, i * tw, j * th, tw, th);
+          break;
+        case NUMNUM:
+          image(grapes, i * tw, j * th, tw, th);
+          break;
+      }
+    }
+  }
+  fill("#000");
+  // ctx.font =  "12px Arial";
+  text("SCORE : " + score, 10, height - 10);
 }
 
 /************************ MAIN p5 FUNCTIONS **********************/
 
 function setup() {
-	createCanvas(col*20, row*20);
-	background(200,123,65);
-	grid.init( EMPTY, col, row);
-	score = 0;
-	let sp = { x:Math.floor(col/2), y:row -1}; 
-	snake.init( LEFT, sp.x, sp.y);
-	grid.set( SNAKE, sp.x, sp.y );
-	setFood( FOOD );
+  createCanvas(col * 20, row * 20);
+  background(200, 123, 65);
+  grid.init(EMPTY, col, row);
+  score = 0;
+  let sp = { x: Math.floor(col / 2), y: row - 1 };
+  snake.init(LEFT, sp.x, sp.y);
+  grid.set(SNAKE, sp.x, sp.y);
+  setFood(FOOD);
 }
 
 function draw() {
+  moveSnake();
+  drawSnake();
 
-	moveSnake();
-	drawSnake();
-
-	// window.requestAnimationFrame( loop, canvas );
+  // window.requestAnimationFrame( loop, canvas );
 }
